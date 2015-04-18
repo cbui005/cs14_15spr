59ad0575f111d84d5a7dbb6fe9e01735dcf013b9
cbui005 <cbui005@ucr.edu>
2015-04-18 21:02:00 +0000

    second commit

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,7 +1,40 @@
+/*
+Name: Calvin Bui
+SID: 861064458
+Date: 4/18
+*/
 #include <iostream>
 #include "lab2.h"
+#include <forward_list>
 using namespace std;
 
+bool isPrime(int i) 
+{
+    int tmp;
+    for (tmp = 2; tmp < i; tmp++)
+    {
+        if (i % tmp == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int primeCount(forward_list<int> lst)
+{
+    if (lst.end() == lst.begin())
+    {
+        return 0;
+    }
+    else if (isPrime(*lst.begin()) == true)
+    {
+        lst.pop_front();
+        return 1 + primeCount(lst);
+    }
+    lst.pop_front();
+    return primeCount(lst);
+}
 
 int main()
 {
@@ -11,7 +44,7 @@ int main()
     list1.push_back(7);
     list1.display();
     cout << endl;
-    list1.elementSwap(list1,0);
+    list1.elementSwap(0);
     list1.display();
     cout << endl;
     return 0;
--- lab2/lab2.h
+++ lab2/lab2.h
@@ -1,6 +1,13 @@
+/*
+Name: Calvin Bui
+SID: 861064458
+Date: 4/18
+*/
+
 #ifndef __LAB2_H__
 #define __LAB2_H__
 #include <iostream>
+#include <forward_list>
 using namespace std;
 
 struct IntNode
@@ -9,6 +16,7 @@ struct IntNode
     IntNode *next;
     IntNode(int data):data(data),next(0) {}
 };
+
 
 template <class Type>
 class List
@@ -20,9 +28,11 @@ class List
         List();
         ~List();
         void display() const;
-        void push_front(int value);
-        void push_back(int value);
-        void elementSwap(List<Type>lst, int pos);
+        void push_front(int data);
+        void push_back(int data);
+        void elementSwap(int pos);
+        forward_list<Type> listCopy(forward_list<Type>L, forward_list<Type>&P);
+        void printLots(forward_list<Type> L, forward_list<int> P);
 };
 
 template <class Type>
@@ -35,7 +45,10 @@ List<Type>::List()
 template <class Type>
 List<Type>::~List()
 {
-    
+    if (head == '\0')
+    {
+        return;
+    }
     for (IntNode* tmp = head; tmp != '\0'; tmp = tmp->next)
     {
         delete tmp;
@@ -63,9 +76,9 @@ void List<Type>::display() const
 }
 
 template <class Type>
-void List<Type>::push_front(int value)
+void List<Type>::push_front(int data)
 {
-    IntNode* tmp = new IntNode(value);
+    IntNode* tmp = new IntNode(data);
     if (head == '\0')
     {
         head = tmp;
@@ -80,9 +93,9 @@ void List<Type>::push_front(int value)
 }
 
 template <class Type>
-void List<Type>::push_back(int value)
+void List<Type>::push_back(int data)
 {
-    IntNode* tmp = new IntNode(value);
+    IntNode* tmp = new IntNode(data);
     if (head == '\0')
     {
         head = tmp;
@@ -96,21 +109,106 @@ void List<Type>::push_back(int value)
 }
 
 template <class Type>
-void List<Type>::elementSwap(List<Type>lst, int pos)
+void List<Type>::elementSwap(int pos)
 {
-    
-    int count = 0;
-    IntNode* tmp;
-    
-    for (tmp = lst.head; count < pos; tmp = tmp->next)
-    {
-        count++;
-    }
-    
-    cout << "tmp:  " << tmp->data << endl;
-    IntNode* tmp2 = tmp;
-    cout << "tmp2: " << tmp2->data << endl;
-
-    
+   IntNode* tmp = head;
+   int count = 0;
+   for (tmp = head; count < pos; count++)
+   {
+       tmp = tmp->next;
+   }
+   cout << "tmp data: " << tmp->data << endl;
+   cout << "next data: " << tmp->next->data << endl;
+   IntNode* tmp2 = tmp->next;
+   if (pos != 0)
+   {
+       IntNode* pred = head;
+       int count = 1;
+       for (pred = head; count < pos; count++)
+       {
+           pred = pred->next;
+       }
+       pred->next = tmp2;
+   }
+   tmp->next = tmp2->next;
+   tmp2->next = tmp;
+   if (pos == 0)
+   {
+       head = tmp2;
+   }
 }
+
+template <class Type>
+forward_list<Type>listCopy(forward_list<Type> L, forward_list<Type>&P)
+{
+    if (L.empty())
+    {
+        if (P.empty())
+        {
+            return P;
+        }
+        for (auto tmp = P.begin(); tmp != P.end(); tmp++)
+        {
+            P.pop_front();
+        }
+        return P;
+    }
+    if (P.empty())
+    {
+        for (auto tmp = L.begin(); tmp != L.end(); tmp++)
+        {
+            P.push_front(*tmp);
+        }
+        return P;
+    }
+    else
+    {
+        for (auto tmp = P.begin(); tmp != P.end(); tmp++)
+        {
+            P.pop_front();
+        }
+        for (auto tmp = L.begin(); tmp != L.end(); tmp++)
+        {
+            P.push_front(*tmp);
+        }
+        return P;
+    }
+}
+
+template <class Type>
+void printLots(forward_list<Type>L, forward_list<int> P)
+{
+    if (L.empty())
+    {
+        return;
+    }
+    int cntr = 0;
+    int sz = 0;
+    auto tmp = L.begin();
+    for (auto tmp = L.begin(); tmp != L.end(); tmp++)
+    {
+        sz++;
+    }
+    P.sort(less<int>());
+    for(auto pos = P.begin(); pos != P.end(); pos++)
+    {
+        if (*pos >= sz)
+        {
+            return;
+        }
+        while(cntr != *pos)
+        {
+            cntr++;
+            tmp++;
+
+        }
+        if(cntr == *pos)
+        {
+            cout << *tmp << " ";
+        }
+
+    }
+}
+
+
 #endif
