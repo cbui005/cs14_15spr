d2fc8a35a9e4f0b325753bfd9f4e7611922d75dd
cbui005 <cbui005@ucr.edu>
2015-05-05 04:53:02 +0000

    lab4

--- lab4/main.cc
+++ lab4/main.cc
@@ -5,18 +5,24 @@ Date: 5/3/15
 */
 #include "lab4.h"
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc != 2)
+    {
+        cout << "Error on arguments";
+        exit(-1);
+    }
     int k = 9;
     cout << "pre-order" << endl;
-    coprime_print_preorder(k,2,1);
-    coprime_print_preorder(k,3,1);
+    coprime_print_preorder(atoi(argv[1]),2,1);
+    coprime_print_preorder(atoi(argv[1]),3,1);
     cout << endl << "printing post order" << endl;
-    coprime_print_postorder(k,2,1);
-    coprime_print_postorder(k,3,1);
+    coprime_print_postorder(atoi(argv[1]),2,1);
+    coprime_print_postorder(atoi(argv[1]),3,1);
     cout << endl << "printing priority sorted" << endl;
 
-    coprime_priority_print(k);
+    coprime_priority_print(atoi(argv[1]));
 }
